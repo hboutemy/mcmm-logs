a481dd65ce51c10f434a91b35a0c45283e7cabe6
Hervé Boutemy <hboutemy@apache.org>
2024-06-09 22:14:30 +0200

    add script to download SBOMs

---
+++ dl-sboms.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+download() {
+  f=$1
+  echo $f $(wc -l < $f)
+  for path in `cat $f`
+  do
+    target=sboms/$path
+    mkdir -p $(dirname $target)
+    [ -f $target ] || ( wget https://repo.maven.apache.org/maven2/$path -O $target -q && du -h $target )
+  done
+}
+
+for format in cyclonedx spdx
+do
+  for f in $format/*/*.txt
+  do
+    download $f
+  done
+done
