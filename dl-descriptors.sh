86db96537e775a6d3bc606c585612acc63f37a60
Hervé Boutemy <hboutemy@apache.org>
2025-05-17 19:02:15 +0000

    2025/05/13

--- dl-descriptors.sh
+++ dl-descriptors.sh
@@ -3,12 +3,12 @@
 # download key descriptors from Maven Central
 
 # - archetype catalog https://maven.apache.org/archetype/archetype-models/archetype-catalog/archetype-catalog.html
-wget https://repo.maven.apache.org/maven2/archetype-catalog.xml -O archetype-catalog.xml
+wget -q https://repo.maven.apache.org/maven2/archetype-catalog.xml -O archetype-catalog.xml
 
 # - org.apache.maven.plugins
-wget https://repo.maven.apache.org/maven2/org/apache/maven/plugins/maven-metadata.xml -O plugins-metadata.xml
+wget -q https://repo.maven.apache.org/maven2/org/apache/maven/plugins/maven-metadata.xml -O plugins-metadata.xml
 # see also https://github.com/hboutemy/mcmm/blob/master/org/apache/maven/plugins/maven-metadata.xml
 
 # - org.codehaus.mojo
-wget https://repo.maven.apache.org/maven2/org/codehaus/mojo/maven-metadata.xml -O mojo-metadata.xml
+wget -q https://repo.maven.apache.org/maven2/org/codehaus/mojo/maven-metadata.xml -O mojo-metadata.xml
 # see also https://github.com/hboutemy/mcmm/blob/master/org/codehaus/mojo/maven-metadata.xml
